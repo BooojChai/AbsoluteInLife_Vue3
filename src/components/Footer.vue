f8d8473a9dfdc88075369f2afa4e48c579d93d32
BooojChai <just_cbj@sina.com>
2022-07-25 22:55:53 +0800

    Add police info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,10 @@
             Powered By <a href="https://v3.cn.vuejs.org" target="_blank">Vue.js</a> | Design & Dev <a href="https://github.com/BooojChai" target="_blank">Boooj</a>
         </div>
         <div class="ending">
-            欢迎常来：）
+          <img src="../assets/batb.png" alt="">
+          <span class="text">
+            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32011402011199" >苏公网安备 32011402011199号</a>
+          </span>
         </div>
     </div>
 </template>
@@ -23,17 +26,35 @@ export default {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
     text-align: center;
     font-size: 15px;
     margin-top: 10px;
     padding-bottom: 20px;
 
-    .power span, a {
-      color: #B7452f;
+    .power {
+      span, a {
+        color: #B7452f;
+      }
     }
 
     .ending {
-      font-size: 14px;
+      margin-top: 10px;
+      padding-top: 10px;
+      width: 300px;
+      font-size: 13px;
+      border-top: 1px solid;
+      border-image: linear-gradient(to right, rgba(66, 66, 66, .1), rgba(66, 66, 66, .9), rgba(66, 66, 66, .1)) 1;
+
+      img {
+        width: 17px;
+        height: 17px;
+      }
+
+      .text {
+        display: inline-block;
+        margin-left: 5px;
+      }
     }
   }
 </style>
